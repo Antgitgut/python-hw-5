1d6cd64893ce77bc9bfeb4164922ef557f502c8c
Antgitgut <antcheery@gmail.com>
2022-07-18 19:30:30 +0300

    Fixed. It seems to be working!

--- Python_hw_2/Excercise_2.py
+++ Python_hw_2/Excercise_2.py
@@ -1,11 +1,15 @@
 def transformStr(str):
     if len(str) > 5:
         print(str[:5] + "...")
-    elif str[:0] == "U" or "u":
-        print(str.upper())
-    elif str[:0] == "L" or "l":
+    elif str[0] in ["L", "l"]:
         print(str.lower())
+    elif str[0] in ["U", "u"]:
+        print(str.upper())
+    else:
+        print(str[:5])
 
 
 
-transformStr("loq")
+
+
+transformStr("posds")
