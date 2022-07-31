7f0e5617cd338824a0148f5cf4f5bc3a60817303
Anthony <97888512+Antgitgut@users.noreply.github.com>
2022-07-31 22:17:29 +0300

    Delete Python_hw_2 directory

--- Python_hw_2/Exercise_1.py
+++
@@ -1,9 +0,0 @@
-def oddOrEven(num):
-    num = int(num)
-    if (num % 2) == 0:
-        print('Число {} є парним'.format(num))
-    else:
-        print('Число {} є не парним'.format(num))
-
-
-oddOrEven(20)
